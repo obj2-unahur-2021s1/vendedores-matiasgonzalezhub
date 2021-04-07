0b02d15bc42673fbbfc28072c1d977b8c9074145
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-04-07 20:17:08 -0300

    etapa 3

--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
@@ -12,4 +12,26 @@ class CentroDistribucion (val vendedores: mutableListOf<Vendedor> , val ciudad :
         vendedores.add(vendedor)
 
     }
+
+    //devuelve vendedor
+    fun vendedorEstrella() : Vendedor
+    {
+        return vendedores.maxBy(this.puntajeCertificaciones())
+    }
+
+    //devuelve bool
+    fun puedeCubrirCiudad(ciudad : Ciudad) : Boolean
+    {
+        return vendedores.any( i -> i.puedeTrabajarEn(ciudad))
+    }
+
+    fun vendedoresGenericos() : mutableListOf<Vendedor>
+    {
+        return vendedores.filter( i -> i.otrasCertificaciones())
+    }
+
+    fun robusto() : Boolean
+    {
+        return vendedores.filter( i -> esFirme()).size > 3
+    }
 }
