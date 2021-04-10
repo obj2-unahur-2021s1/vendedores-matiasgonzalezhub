1258ae956fff36dfc8f781b53079d406ef8c90c9
Matias Gonzalez <matias.gonzalez@live.com.ar>
2021-04-10 12:01:49 -0300

    centro de distribuciones

---
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
@@ -0,0 +1,32 @@
+package ar.edu.unahur.obj2.vendedores
+
+class CentroDistribucion (val ciudad: Ciudad){
+
+    val vendedores = mutableListOf<Vendedor>()
+
+    fun agregarVendedor(vendedor : Vendedor){
+        vendedores.add(vendedor)
+    }
+
+    fun vendedorEstrella() : Vendedor? {
+      return   vendedores.maxBy { c -> c.puntajeCertificaciones() }
+    }
+
+    fun puedeCubri(ciudad : Ciudad) : Boolean{
+        return vendedores.any { i -> i.puedeTrabajarEn(ciudad) }
+    }
+
+    fun vendedoresGenericos() : List<Vendedor>
+    {
+        return vendedores.filter { c -> c.otrasCertificaciones() == 1 }
+    }
+
+    fun esRobusto() : Boolean {
+
+        var vendedoresFirmes = mutableListOf<Vendedor>()
+
+        vendedoresFirmes = vendedores.filter { c: Vendedor -> c.esFirme() }.toMutableList()
+
+        return vendedoresFirmes.size >= 3
+    }
+}
